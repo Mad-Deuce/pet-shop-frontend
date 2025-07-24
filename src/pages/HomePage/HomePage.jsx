f841c926c396fc16b0d7a255c43e95632ca7c2cc
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-07-25 00:04:43 +0300

    refactoring

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,29 @@
+import { useCallback } from "react";
+
 import DiscountBanner from "/src/modules/DiscountBanner/DiscountBanner";
 import Categories from "/src/modules/Categories/Categories";
 import FirstOrderBanner from "/src/modules/FirstOrderBanner/FirstOrderBanner";
 import Products from "/src/modules/Products/Products";
 
+import Output from "/src/shared/components/Output/Output";
 import Container from "/src/shared/components/Container/Container";
 import SectionTitle from "/src/shared/components/SectionTitle/SectionTitle";
+
+import useFetch from "/src/shared/hooks/useFetch";
+import { getPopularCategoriesApi } from "/src/shared/api/categoriesApi";
 
 import styles from "./HomePage.module.css";
 
 export default function HomePage() {
+  const {
+    state: categories,
+    error: categoriesError,
+    loading: categoriesLoading,
+  } = useFetch({
+    request: useCallback(() => getPopularCategoriesApi(), []),
+    initialState: [],
+  });
+
   return (
     <div className={styles.homePage}>
       <DiscountBanner />
@@ -21,13 +36,17 @@ export default function HomePage() {
           >
             Categories
           </SectionTitle>
-          <Categories />
-          <FirstOrderBanner />
-          <SectionTitle
-            showButton
-            buttonLabel="All Sales"
-            buttonLink="/sales"
+          <Output
+            error={categoriesError}
+            loading={categoriesLoading}
+            condition={true}
+            altMessage="No popular categories available"
           >
+            <Categories categories={categories} />
+          </Output>
+
+          <FirstOrderBanner />
+          <SectionTitle showButton buttonLabel="All Sales" buttonLink="/sales">
             Sale
           </SectionTitle>
           <Products />
--- src/shared/api/categoriesApi.js
+++ src/shared/api/categoriesApi.js
@@ -3,8 +3,12 @@ import axios from "axios";
 import { URL } from "./config.js"
 import { fetchDecorator } from "/src/shared/lib/fetchDecorator";
 
-const instance = axios.create({ baseURL: URL + "categories" });
+const instance = axios.create({ baseURL: URL });
 
-export const getAllCategoriesApi = fetchDecorator(() => instance.get("/all"));
+const defaultParams = { page: 1, perPage: 100 }
 
-export const getProductsByCategoryIdApi = fetchDecorator((id) => instance.get(`/${id}`));
+export const getAllCategoriesApi = fetchDecorator((params = defaultParams) => instance.get("categories/all", { params: { ...params } }));
+
+export const getPopularCategoriesApi = fetchDecorator((params = defaultParams) => instance.get("categories/all", { params: { page: 1, perPage: 4 } }));
+
+export const getProductsByCategoryIdApi = fetchDecorator((id) => instance.get(`categories/${id}`));
