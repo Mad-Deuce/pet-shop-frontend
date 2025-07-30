1e6a0ac969ea198094a3ecf2ab1126178d25fce6
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-07-30 23:42:08 +0300

    solved problem with default params in productsApi

--- src/shared/api/productsApi.js
+++ src/shared/api/productsApi.js
@@ -8,14 +8,14 @@ const instance = axios.create({ baseURL });
 
 const defaultParams = { page: 1, perPage: 100 }
 
-export const getAllProductsApi = fetchDecorator(({ params = defaultParams }) => instance.get("products/all", { params: { ...params } }));
+export const getAllProductsApi = fetchDecorator(({ params = {} }) => instance.get("products/all", { params: { ...params, ...defaultParams } }));
 
 export const getPopularProductsApi = fetchDecorator(() => instance.get("products/all", { params: { discont: true, page: 1, perPage: 4 } }));
 
-export const getDiscontedProductsApi = fetchDecorator(({ params = defaultParams }) => instance.get("products/all", { params: { discont: true, ...params } }));
+export const getDiscontedProductsApi = fetchDecorator(({ params = {} }) => instance.get("products/all", { params: { discont: true, ...params, ...defaultParams } }));
 
-export const getProductsByCategoryApi = fetchDecorator(({ categorySlug, params = defaultParams }) => {
-    return instance.get(`categories/${categorySlug}`, { params: { ...params } })
+export const getProductsByCategoryApi = fetchDecorator(({ categorySlug, params = {} }) => {
+    return instance.get(`categories/${categorySlug}`, { params: { ...params, ...defaultParams } })
 });
 
 export const getProductByIdApi = fetchDecorator((productId) => instance.get("products/" + productId));
